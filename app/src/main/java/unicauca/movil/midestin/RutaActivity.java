96b0ba61b731d3b4297fde6403cf54929223cf6f
Katherin Escobar <escobarkathe@gmail.com>
2016-12-15 20:36:43 -0500

    Rura 80

--- app/src/main/java/unicauca/movil/midestin/RutaActivity.java
+++ app/src/main/java/unicauca/movil/midestin/RutaActivity.java
@@ -1,7 +1,12 @@
 package unicauca.movil.midestin;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,24 +14,35 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import unicauca.movil.midestin.databinding.ActivityRutaBinding;
+import unicauca.movil.midestin.models.Ciudad;
 
 /**
  * Created by Kathe on 15/12/2016.
  */
 
-public class RutaActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
-  ListView listView=null;
+public class RutaActivity extends AppCompatActivity implements DialogInterface.OnClickListener, DatePickerDialog.OnDateSetListener {
+    ActivityRutaBinding binding;
     static String[] ciudades = {"Cali", "Bogota","Popayan"};
     Button btn;
     AlertDialog ad;
+    Ciudad ciudad;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ruta);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_ruta);
+
 
         //Titulo e inicializacion
         setTitle("   COMPRAR O RESERVAR TIQUETE");
@@ -36,6 +52,7 @@ public class RutaActivity extends AppCompatActivity implements DialogInterface.O
              ad.show();
             }
         });
+
         AlertDialog.Builder builder= new AlertDialog.Builder(this);
         builder.setTitle("            Selecciona la Ciudad ");
         builder.setItems(ciudades, this);
@@ -48,6 +65,40 @@ public class RutaActivity extends AppCompatActivity implements DialogInterface.O
     @Override
     public void onClick(DialogInterface dialog, int pos) {
           String selectedItem=ciudades[pos];
-         Toast.makeText(this,selectedItem,Toast.LENGTH_SHORT).show();
+          Toast.makeText(this,selectedItem,Toast.LENGTH_SHORT).show();
+         ((TextView) findViewById(R.id.ori)).setText(selectedItem);
+    }
+
+    public void datePicker(View view){
+        DatePickerFragment fragment =new DatePickerFragment();
+        fragment.show(getFragmentManager(),"date");
+
+    }
+
+    private void setDate(final Calendar calendar){
+        final DateFormat dateFormat= DateFormat.getDateInstance(DateFormat.MEDIUM);
+        ((TextView) findViewById(R.id.fec)).setText(dateFormat.format(calendar.getTime()));
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar cal= new GregorianCalendar(year,month,day);
+        setDate(cal);
+    }
+
+    public static  class DatePickerFragment extends DialogFragment{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c=Calendar.getInstance();
+            int year= c.get(Calendar.YEAR);
+            int month= c.get(Calendar.MONTH);
+            int day= c.get(Calendar.DAY_OF_MONTH);
+
+
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener)getActivity(),year,month,day) ;
+        }
     }
 }
---
+++ app/src/main/java/unicauca/movil/midestin/models/Ciudad.java
@@ -0,0 +1,28 @@
+package unicauca.movil.midestin.models;
+
+/**
+ * Created by Kathe on 15/12/2016.
+ */
+
+public class Ciudad {
+    private int id;
+    private String nombre;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
